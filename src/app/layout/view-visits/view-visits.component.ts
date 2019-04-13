4eef18411dec1cef05b4931ba899740356c57f32
Jessica <jessicamrey@udc.es>
2019-04-13 14:12:27 +0200

    Continuando apartado mis visitas

--- src/app/layout/view-visits/view-visits.component.ts
+++ src/app/layout/view-visits/view-visits.component.ts
@@ -20,11 +20,15 @@ export class ViewVisitsComponent implements OnInit {
 
 	colonias:any=[];
 	coloniasId:any=[];
+  territorios:any=[];
+  territoriosId:any=[];
 
   showCol:boolean=true;
 
   dateSince:any;
   dateUntil:any;
+
+  totalPages:any=0;
 
   	constructor(private translate: TranslateService,
                 private coloniasService: ColoniasService,
@@ -38,11 +42,22 @@ export class ViewVisitsComponent implements OnInit {
   	}
 
   	recuperaVisitas(user){
-  		this.coloniasService.recuperaVisitasGeneral('?usuario=' + user).subscribe(
+  		this.coloniasService.recuperaVisitasGeneral('?usuario=' + user ).subscribe(
                         data =>{
                         	this.listaVisitas=data["hydra:member"];
-                            console.log(data);
+
+                          let last=data["hydra:view"]["hydra:last"];
+                          last=last.substr(last.indexOf('page')+5); //Cogemos el substring a partir de page +5, es decir +4 (numero de letras de page) +1 para no coger el "=", es decir, +5
+                          this.totalPages=last*10;
+
                             for (let item of data["hydra:member"]){
+                              let date=new Date((item["fecha"]));
+                              let y=date.getFullYear();
+                              let m=date.getMonth()+1;
+                              let d=date.getUTCDate()+1;
+
+                              item["fecha"]=d +'/' + m + '/' + y;
+
                             	if (this.coloniasId.indexOf(item.colonia["id"])<0){
                             		this.colonias.push(item.colonia);
                             		this.coloniasId.push(item.colonia["id"]);
@@ -61,15 +76,13 @@ export class ViewVisitsComponent implements OnInit {
       this.territoriosService.recuperaVisitasGeneral('?usuario='+user).subscribe(
                         data =>{
 
-                          console.log(data);
-
-
-                          this.listaVisitasTerr=[];
+                          
+                          this.listaVisitasTerr=data["hydra:member"];
                           for (let visita of data["hydra:member"]){
                             let date=new Date((visita["fecha"]));
                             let y=date.getFullYear();
                             let m=date.getMonth()+1;
-                            let d=date.getUTCDate();
+                            let d=date.getUTCDate()+1;
 
                             let h=date.getHours();
                             let min=date.getMinutes();
@@ -79,35 +92,138 @@ export class ViewVisitsComponent implements OnInit {
                             visita["hora"]=h +':' + min + ':' + s;
 
                             this.listaVisitasTerr.push(visita);
+
+                            if (this.territoriosId.indexOf(visita.territorio["id"])<0){
+                                this.territorios.push(visita.territorio);
+                                this.territoriosId.push(visita.territorio["id"]);
+
+                              }
+
+
                           }
                         },
                         error=>{
                             console.log(error);
                         });
+
+      console.log(this.listaVisitasTerr);
     }
 
-  	filtrar(coloniaId, user=0){
+  	filtrar(user=0, page){
       let id= $( "#colonia option:selected" ).attr("id");
-      if (id=="all"){
-        this.recuperaVisitas(user);
-      }else{
-        this.coloniasService.recuperaVisitasGeneral('?colonia='+id + '&usuario=' + user).subscribe(
+     
+      let busqueda='?usuario='+user;
+
+      if (id!="all"){
+        busqueda=busqueda+'&colonia='+id;
+        
+      }
+       this.dateSince!=undefined ? busqueda=busqueda+'&fecha[after]='+this.dateSince.year+'-'+this.dateSince.month+'-'+this.dateSince.day : busqueda;
+       this.dateUntil!=undefined ? busqueda=busqueda+'&fecha[before]='+this.dateUntil.year+'-'+this.dateUntil.month+'-'+this.dateUntil.day : busqueda;
+
+       page ? busqueda=busqueda+'&page='+page : busqueda;
+
+      this.listaVisitas=[];
+      this.coloniasService.recuperaVisitasGeneral(busqueda).subscribe(
                         data =>{
-                          this.listaVisitas=data["hydra:member"];
+                         
+
+                          let last=data["hydra:view"]["hydra:last"];
+                          last=last.substr(last.indexOf('page')+5); //Cogemos el substring a partir de page +5, es decir +4 (numero de letras de page) +1 para no coger el "=", es decir, +5
+                          this.totalPages=last*10;
+
+                          for (let visita of data["hydra:member"]){
+                            let date=new Date((visita["fecha"]));
+                            let y=date.getFullYear();
+                            let m=date.getMonth()+1;
+                            let d=date.getUTCDate()+1;
+
+                            visita["fecha"]=d +'/' + m + '/' + y;
+
+                            this.listaVisitas.push(visita);
+
+                          }
+
+
 
                         },
                         error=>{
                             console.log(error);
                         });
-      }
   		
   	}
+
+
+
+
+    filtrarTerr(user=0, page){
+      let id= $( "#territorio option:selected" ).attr("id");
+     
+      let busqueda='?usuario='+user;
+
+      if (id!="all"){
+        busqueda=busqueda+'&territorio='+id;
+        
+      }
+       this.dateSince!=undefined ? busqueda=busqueda+'&fecha[after]='+this.dateSince.year+'-'+this.dateSince.month+'-'+this.dateSince.day : busqueda;
+       this.dateUntil!=undefined ? busqueda=busqueda+'&fecha[before]='+this.dateUntil.year+'-'+this.dateUntil.month+'-'+this.dateUntil.day : busqueda;
+
+      page ? busqueda=busqueda+'&page='+page : busqueda;
+
+      this.listaVisitasTerr=[];
+
+      this.territoriosService.recuperaVisitasGeneral(busqueda).subscribe(
+                        data =>{
+                         
+
+                          let last=data["hydra:view"]["hydra:last"];
+                          last=last.substr(last.indexOf('page')+5); //Cogemos el substring a partir de page +5, es decir +4 (numero de letras de page) +1 para no coger el "=", es decir, +5
+                          this.totalPages=last*10;
+
+                          for (let visita of data["hydra:member"]){
+                            let date=new Date((visita["fecha"]));
+                            let y=date.getFullYear();
+                            let m=date.getMonth()+1;
+                            let d=date.getUTCDate()+1;
+
+                            let h=date.getHours();
+                            let min=date.getMinutes();
+                            let s=date.getSeconds();
+
+                            visita["fecha"]=d +'/' + m + '/' + y;
+                            visita["hora"]=h +':' + min + ':' + s;
+
+
+                            this.listaVisitasTerr.push(visita);
+
+                          }
+
+
+
+                        },
+                        error=>{
+                            console.log(error);
+                        });
+      
+    }
 
 
   	openLg(content) {
     this.modalService.open(content, { size: 'lg' });
   }
 
+  limpiar(){
+    this.dateSince=undefined;
+    this.dateUntil=undefined;
+  }
+
+  pageChanged(page) {
+    this.filtrar(0,page);
+  }
+
+  pageChangedTerr(page) {
+    this.filtrarTerr(0,page);
+  }
   	
           
 	
