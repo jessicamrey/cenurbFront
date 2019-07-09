ba8904c1378b2140f228eb930134422fc7d79e97
jessicamrey <30998957+jessicamrey@users.noreply.github.com>
2019-07-09 11:52:51 +0200

    Update censo-municipio.component.ts

--- src/app/layout/censo-municipio/censo-municipio.component.ts
+++ src/app/layout/censo-municipio/censo-municipio.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { SeoApisService } from '../../../services/seo-apis.service';
+import { ColoniasService } from '../../../services/colonias.service';
+import { TranslateService } from '@ngx-translate/core';
+import { AlertService } from 'ngx-alerts';
+
+declare var $:any;
 
 @Component({
   selector: 'app-censo-municipio',
@@ -7,9 +14,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CensoMunicipioComponent implements OnInit {
 
-  constructor() { }
+  listaCCAA:any[]= [];
+  listaTemporadas:any[]= [];
+  listaProv:any[]= [];
+  listaMun:any[]= [];  
+  listaColDisponibles:any[]= [];
+  listaColAsignadas:any[]= [];
+    
+  constructor(private translate: TranslateService,
+                private seoService: SeoApisService,
+                private coloniasService: ColoniasService,
+                public alertService: AlertService) { }
 
   ngOnInit() {
   }
+  
+   recuperaTemporadas(){
+    this.coloniasService.getTemporadas().subscribe(
+      data=>{
+        
+        for (let item of data){
+          
+            this.listaTemporadas.push(item["anno"]);
+          
+        }
+        console.log(this.listaTemporadas);
+      },
+      error=>{
+        console.log(error);
+      })
+  }
 
+  
+   recuperaCCAA(){
+  	this.seoService.getCCAA().subscribe(
+              data => {
+                this.listaCCAA=data;
+              },
+              error => {
+                  this.alertService.warning(this.translate.instant("Dashboard.errorGetCCAA"));
+                  
+            }
+        );
+  }
+  
+   recuperaProvincia(){
+  	this.listaProv= [];
+  	this.listaMun= [];
+  	let id= $( "#selectCCAA option:selected" ).attr("id");
+  	this.seoService.getProvincia(id).subscribe(
+              data => {
+                this.listaProv=data;
+              },
+              error => {
+                  this.alertService.warning(this.translate.instant("Dashboard.errorGetProv"));
+                  
+            }
+        );
+
+  }
+
+  recuperaMunicipio(){
+  	let id= $( "#selectProvincia option:selected" ).attr("id");
+  	this.seoService.getMunicipio(id).subscribe(
+              data => {
+                this.listaMun=data;
+              },
+              error => {
+                  this.alertService.warning(this.translate.instant("Dashboard.errorGetMun"));
+                  
+            }
+        );
+
+  }
+  
+  buscar(){
+    //llamar a la api y asignar variables
+  
+  }
+  
+  editar(){
+   //cambios en campo completo de cada colonia, por lo que editar colonia
+    //cambios de campo completo de municpio, opr lo que editar censomucnipio
+    //cambiso en colonias asignadas y disponibles, editar censomunicipio
+  }
 }
