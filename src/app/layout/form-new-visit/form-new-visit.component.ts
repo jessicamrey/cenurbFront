4b0ca5cb881b5baa08b4521d40f7fcd19b54a123
jessicamrey <30998957+jessicamrey@users.noreply.github.com>
2019-07-16 08:52:21 +0200

    Update form-new-visit.component.ts

--- src/app/layout/form-new-visit/form-new-visit.component.ts
+++ src/app/layout/form-new-visit/form-new-visit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-form-new-visit',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-new-visit.component.scss']
 })
 export class FormNewVisitComponent implements OnInit {
-
+  url:any;
   constructor() { }
 
   ngOnInit() {
+    this.url = environment.backendUrl + '/admin';
   }
 
 }
